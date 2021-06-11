22863a492b3390d2cdbe4397cfe8b4799dfaa9d1
Kshitiz Gupta <44299394+kshitiz045@users.noreply.github.com>
2021-06-11 22:49:28 +0530

    Add files via upload

---
+++ src/comps/Client.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function Client() {
+    return (
+        <div className="clients">
+            <div className="container">
+                <div className="row">
+                    <div className="col-3">
+                        <div className="client">
+                            <h1>120</h1>
+                            <p>Awesome Employees</p>                       </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="client">
+                            <h1>58</h1>
+                            <p>Pizza Types</p>                       </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="client">
+                            <h1>14868</h1>
+                            <p>Satisfied Clients</p>                       </div>
+                    </div>
+                    <div className="col-3">
+                        <div className="client">
+                            <h1>2031</h1>
+                            <p>Delivery</p>                       </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Client 
---
+++ src/comps/Shop.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Shop() {
+    return (
+        <div className="shop">
+            <div className="shop__text">
+                <div>
+                    <h1>Free Delivery with PIZZA OF THE DAY Only 8.99 USD</h1>
+                    <div className="shop__btn">
+                        <a href="" className="btn btn-smart">
+                            SHOP NOW
+                        </a>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Shop
